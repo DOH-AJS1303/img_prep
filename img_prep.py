4ca5f4072af6abd7a9bda69b2f79ba0ade07a926
Andrew Sikes <81252301+DOH-AJS1303@users.noreply.github.com>
2023-01-18 18:38:45 -0800

    Update img_prep.py

--- img_prep.py
+++ img_prep.py
@@ -4,14 +4,13 @@ import os
 img_loc = ""
 sav_loc = img_loc + "Batch\\"
 size = 600,800
+images = os.listdir(img_loc)
 
 def process(img_name, img_path, sav_loc, size):
     img = Image.open(img_path + img_name)
     img = img.rotate(90, expand=True)
     img = ImageOps.pad(image=img, size=size, method=Image.Resampling.LANCZOS)
     img.save(sav_loc + img_name)
-   
-images = os.listdir(img_loc)
 
 for img_name in images:
     process(img_name, img_loc, sav_loc, size)
